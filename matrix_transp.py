387446a7c4ae2039d48244bb35e3775c0a3e264e
walkingscript <alexander.bondarenko2016@gmail.com>
2020-10-12 14:30:39 +0300

    Task completed.

---
+++ matrix_transp.py
@@ -0,0 +1,37 @@
+"""Транспонирование матрицы
+На вход поступают размеры матрицы n и m.
+Затем построчно матрица.
+Результат: транспонированная матрица."""
+
+
+def get_matrix(count_of_lines: int) -> list:
+    matrix = []
+    for i in range(count_of_lines):
+        matrix.append(list(map(int, input().split())))
+    return matrix
+
+
+def transp(matrix: list) -> list:
+    n, m = len(matrix), len(matrix[0])
+    res_mat = [[] for i in range(m)]
+    for line in matrix:
+        for number, item in enumerate(line):
+            res_mat[number].append(item)
+    return res_mat
+
+
+def print_mat(matrix):
+    for line in matrix:
+        mat_str = map(str, line)
+        print(' '.join(mat_str))
+
+
+def main():
+    n, m = map(int, input().split(' '))
+    mat = get_matrix(n)
+    transp_mat = transp(mat)
+    print_mat(transp_mat)
+    
+
+if __name__ == '__main__':
+    main()
