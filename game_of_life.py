20662cef8ec16d215384403318046f3c9822669a
walkingscript <alexander.bondarenko2016@gmail.com>
2020-10-14 00:05:44 +0300

    Need fixes

--- game_of_life.py
+++ game_of_life.py
@@ -1,38 +1,56 @@
-neibours = ((0, 1), (1, 1), (1, 0), (1, -1), 
-            (0, -1), (-1, -1), (-1, 0), (-1, 1))
-n, m = map(int, input().split())
-field = []
-for i in range(n):
-    line = list(input())
-    if len(line) == m:
-        field.append(line)
-    else:
-        raise ValueError
+from loguru import logger
 
-next_field = []
 
-for i in range(n):
-    for j in range(m):
-        count_live_cells = 0
-        for x, y in neibours:
-            try:
-                pos1 = i + x
-                pos2 = j + y
-                if pos1 < 0 or pos2 < 0:
+logger.add('log')
+
+
+def print_field(field: list) -> None:
+    for line in field:
+        print(''.join(line))
+
+
+def get_next_field(field: list, size: tuple) -> list:
+    n, m = size
+    neighbour_cells = ((0, 1), (1, 1), (1, 0), (1, -1),
+                     (0, -1), (-1, -1), (-1, 0), (-1, 1))
+    next_field = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            neighbors = []
+            for x, y in neighbour_cells:
+                try:
+                    pos1 = i + x
+                    if pos1 == n:
+                        pos1 = -1
+                    pos2 = j + y 
+                    if pos2 == m:
+                        pos2 = -1
+                    neighbors.append(field[pos1][pos2])
+                except IndexError:
                     continue
-                if field[pos1][pos2] == 'X':
-                    count_live_cells += 1
-            except IndexError:
-                continue
-        # в мёртвой клетке, вокруг которой ровно 3 живые клетки - зарождается жизнь
-        if field[i][j] == '.' and count_live_cells == 3:
-            next_field.append('X')
-        # если у живой клетки есть две или три живые соседки
-        elif field[i][j] == 'X' and count_live_cells not in (2, 3):
-            #то эта клетка продолжает жить
-            next_field.append('.')
-        else:
-            next_field[i].append(field[i][j])
+            state = field[i][j]
+            if state == '.' and neighbors.count('X') == 3:
+                next_field[i].append('X')
+            elif state == 'X' and neighbors.count('X') < 2 or neighbors.count('X') > 3:
+                next_field[i].append('.')
+            else:
+                next_field[i].append(state)
+    return next_field
+    
 
-for line in next_field:
-    print(''.join(line))
+def main():
+    n, m = map(int, input().split())
+    field = []
+    for _ in range(n):
+        line = list(input())
+        if len(line) == m:
+            field.append(line)
+        else:
+            raise ValueError
+    print_field(field)
+    next_field = get_next_field(field, (n, m))
+    print_field(next_field)
+
+
+if __name__ == "__main__":
+    main()
