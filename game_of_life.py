23a1c8a6ed2fbaa761d45f5b7042d84b30b99a34
walkingscript <alexander.bondarenko2016@gmail.com>
2020-10-15 14:58:01 +0300

    pep8 applied.

--- game_of_life.py
+++ game_of_life.py
@@ -6,7 +6,7 @@ def print_field(field: list) -> None:
 def get_next_field(field: list, size: tuple) -> list:
     n, m = size
     neighbour_cells = ((0, 1), (1, 1), (1, 0), (1, -1),
-                     (0, -1), (-1, -1), (-1, 0), (-1, 1))
+                       (0, -1), (-1, -1), (-1, 0), (-1, 1))
     next_field = [[] for _ in range(n)]
     for i in range(n):
         for j in range(m):
@@ -25,7 +25,8 @@ def get_next_field(field: list, size: tuple) -> list:
             state = field[i][j]
             if state == '.' and neighbors.count('X') == 3:
                 next_field[i].append('X')
-            elif state == 'X' and neighbors.count('X') < 2 or neighbors.count('X') > 3:
+            elif state == 'X' and neighbors.count('X') < 2 \
+                              or neighbors.count('X') > 3:
                 next_field[i].append('.')
             else:
                 next_field[i].append(state)
